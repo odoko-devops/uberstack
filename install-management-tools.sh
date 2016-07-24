c91a8dc854d27c2ae2b7ef51fc4743c3243e1d87
Upayavira <uv@odoko.co.uk>
2016-07-24 19:37:30 +0100

    Make mgt box docker listen over tcp so Jenkins can use it

--- install-management-tools.sh
+++ install-management-tools.sh
@@ -4,6 +4,11 @@ cd /tmp
 curl -Ls https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > docker-compose
 chmod +x docker-compose
 
+sudo bash -c "echo 'DOCKER_OPTS=\"-H tcp://172.17.0.1:4243 -H unix:///var/run/docker.sock\"' >> /etc/default/docker"
+sleep 5
+sudo service docker restart
+sleep 15 # wait so that Docker has properly restarted before Jenkins attempts to log-in
+
 export DOMAIN=$1
 export EMAIL=$2
 export USERNAME=$3
