0e6b9200d91568209a53a04dd359eb71aee90573
Upayavira <uv@odoko.co.uk>
2016-09-08 21:57:08 +0100

    Add a Security group for the Docker host

---
+++ container/terraform/aws/docker.tf
@@ -0,0 +1,52 @@
+resource "aws_security_group" "management" {
+  name = "docker-hosts"
+  description = "Security group for Docker Hosts"
+  vpc_id = "${aws_vpc.default.id}"
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = "8983"
+    to_port     = "443"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = "2376"
+    to_port     = "2376"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags { 
+    Name = "docker-hosts" 
+  }
+}
+
