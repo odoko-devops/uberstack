c88dabfc4e33e601424da2a4c80418018821c13b
Upayavira <uv@odoko.co.uk>
2016-09-13 11:05:01 +0100

    Write a local file so that VirtualBox remote code has shell access

--- src/remote/apps/virtualbox.go
+++ src/remote/apps/virtualbox.go
@@ -1,27 +1,40 @@
 package apps
 
 import (
-	"fmt"
 	"utils"
+	"text/template"
+	"io/ioutil"
+	"bytes"
 )
 
+var virtualBoxInstallScript=`#!/bin/sh
+sudo mkdir /mnt/sda1/var/lib/rancher
+echo 'sudo mkdir /mnt/sda1/var/lib/rancher' | sudo tee -a /var/lib/boot2docker/profile
+echo 'sudo mount -r /mnt/sda1/var/lib/rancher /var/lib/rancher' | sudo tee -a /var/lib/boot2docker/profile
+echo '{{.ip}} netmask {{.netmask}} broadcast {{.broadcast}}' | sudo tee /etc/ip.cfg
+echo 'sudo cat /var/run/udhcpc.eth1.pid | xargs sudo kill' | sudo tee -a /var/lib/boot2docker/bootsync.sh
+echo 'sudo ifconfig eth1 $(cat /etc/ip.cfg) up' | sudo tee -a /var/lib/boot2docker/bootsync.sh
+sudo cat /var/run/udhcpc.eth1.pid | xargs sudo kill
+sudo ifconfig eth1 $(cat /etc/ip.cfg) up
+`
 func Virtualbox_Configure(ip, netmask, broadcast string) {
 
-	commands := []string{
-		"sudo mkdir /mnt/sda1/var/lib/rancher",
-		"echo 'sudo mkdir /mnt/sda1/var/lib/rancher' | sudo tee -a /var/lib/boot2docker/profile",
-		"echo 'sudo mount -r /mnt/sda1/var/lib/rancher /var/lib/rancher' | sudo tee -a /var/lib/boot2docker/profile",
-		fmt.Sprintf("echo '%s netmask %s broadcast %s' | sudo tee /etc/ip.cfg", ip, netmask, broadcast),
-		"echo 'sudo cat /var/run/udhcpc.eth1.pid | xargs sudo kill' | sudo tee -a /var/lib/boot2docker/bootsync.sh",
-		"echo 'sudo ifconfig eth1 $(cat /etc/ip.cfg) up' | sudo tee -a /var/lib/boot2docker/bootsync.sh",
-		"sudo cat /var/run/udhcpc.eth1.pid | xargs sudo kill",
-		"sudo ifconfig eth1 $(cat /etc/ip.cfg) up",
-	}
+	virtualboxTemplate, err := template.New("terraformEip").Parse(virtualBoxInstallScript)
+	utils.Check(err)
 
-	for _, command := range commands {
-		fmt.Printf("Execute: %s\n", command)
-		utils.Execute(command, nil, "")
+	var buf bytes.Buffer
+
+
+	params := map[string]string{
+		"ip": ip,
+		"netmask": netmask,
+		"broadcast": broadcast,
 	}
+	err = virtualboxTemplate.Execute(&buf, params)
+	utils.Check(err)
+	err = ioutil.WriteFile("/tmp/virtualbox.sh", buf.Bytes(), 0755)
+	utils.Check(err)
+	utils.Execute("/tmp/virtualbox.sh", nil, "")
 }
 
 
